5860b84a90e85c41d3fbfbf684859a3aaff39b05
凌晨 <lingchen0617@sina.com>
2020-02-09 19:21:50 +0800

    合并K个排序链表

---
+++ LeetCode/022括号生成/GenerateParenthesis.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public partial class Solution
+    {
+        public IList<string> GenerateParenthesis(int n) 
+        {
+            IList<string> result = null;
+            if (n == 0)
+            {
+                return new List<string>();
+            }
+            int cl = 0;//左括号计数
+            int cr = 0;//右括号计数
+            result = GenerateParenthesis(n, cl, cr);
+            return result;
+        }
+
+        private IList<string> GenerateParenthesis(int n, int cl, int cr)
+        {
+            IList<string> result = new List<string>();
+            if (cl > cr && cl < n)//条件1
+            {
+                IList<string> nextL = GenerateParenthesis(n, cl + 1, cr);
+                IList<string> nextR = GenerateParenthesis(n, cl, cr + 1);
+                for (int i = 0; i < nextL.Count; i++)
+                {
+                    result.Add($"({nextL[i]}");
+                }
+                for (int i = 0; i < nextR.Count; i++)
+                {
+                    result.Add($"){nextR[i]}");
+                }
+                return result;
+            }
+            else if (cl == cr && cl < n)//条件2
+            {
+                IList<string> nextL = GenerateParenthesis(n, cl + 1, cr);
+                for (int i = 0; i < nextL.Count; i++)
+                {
+                    result.Add($"({nextL[i]}");
+                }
+                return result;
+            }
+            else if (cl > cr && cl == n)//条件3
+            {
+                IList<string> nextR = GenerateParenthesis(n, cl, cr + 1);
+                for (int i = 0; i < nextR.Count; i++)
+                {
+                    result.Add($"){nextR[i]}");
+                }
+                return result;
+            }
+            else//条件4
+            {
+                result.Add("");
+                return result;
+            }
+        }
+    }
+}
---
+++ LeetCode/023合并K个排序链表/MergeKLists.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public partial class Solution
+    {
+        public ListNode MergeKLists(ListNode[] lists)
+        {
+            int lenght = lists.Length;
+            if (lenght == 0) 
+            {
+                return null;
+            }
+            while (lenght > 1)
+            {
+                for (int i = 0; i < lenght / 2; i++)
+                {
+                    lists[i] = MergeTwoLists(lists[i], lists[lenght/2 + i]);
+                }
+                lenght = lenght/ 2;
+            }
+            return lists[0];
+
+        }
+    }
+}
