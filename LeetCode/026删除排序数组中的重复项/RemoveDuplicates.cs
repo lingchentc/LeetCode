23cc60442923609b40914ba487a4a49770d316c6
凌晨 <lingchen0617@sina.com>
2020-02-12 15:43:08 +0800

    删除排序数组中的重复项

---
+++ LeetCode/026删除排序数组中的重复项/RemoveDuplicates.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public partial class Solution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums == null)//空数组返回0
+            {
+                return 0;
+            }
+            if (nums.Length <= 1)//数组长度小于等于1 返回数组长度（0和1长度的数组都不会有重复项目）
+            {
+                return nums.Length;
+            }
+            int j = 0;
+            for (int i = 1; i < nums.Length; i++)//指针i用于遍历数组
+            {
+                if (nums[i] != nums[j])
+                {
+                    j++;//指针j，用于记录位置
+                    nums[j] = nums[i];
+                }
+            }
+            return j + 1;//数组的长度为j+1
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -9,12 +9,8 @@ namespace LeetCode
         {
 
             Solution s = new Solution();
-            ListNode listNode = new ListNode(1);
-            listNode.next = new ListNode(2);
-            listNode.next.next = new ListNode(3);
-            listNode.next.next.next = new ListNode(4);
-            listNode.next.next.next.next = new ListNode(5);
-            s.ReverseKGroup(listNode, 2);
+
+            s.RemoveDuplicates(new int[] { 1,1,2});
             Console.WriteLine("Hello World");
         }
     }
