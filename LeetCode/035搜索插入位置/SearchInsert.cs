d2d9e57347a374e35aa95c873175af9b7887ed58
凌晨 <lingchen0617@sina.com>
2020-02-26 18:48:21 +0800

    有效的数独

---
+++ LeetCode/035搜索插入位置/SearchInsert.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public partial class Solution
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            //特例
+            if (nums[0] >= target) 
+            {
+                return 0;
+            }
+            if (nums[nums.Length - 1] == target) 
+            {
+                return nums.Length -1;
+            }
+            if (nums[nums.Length - 1] < target) 
+            {
+                return nums.Length;
+            }
+            int front = 0, end = nums.Length - 1;
+           
+            while (front < end) //找到目标
+            {
+                int mid = (front + end) / 2;
+                if (nums[mid] > target)
+                {
+                    end = mid - 1;
+                }
+                else if (nums[mid] < target)
+                {
+                    front = mid + 1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+            if (nums[front] >= target) 
+            {
+                return front;
+            }
+            if (nums[front] < target) 
+            {
+                return front + 1;
+            }
+            return 0;
+        }
+    }
+}
---
+++ LeetCode/036有效的数独/IsValidSudoku.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public partial class Solution
+    {
+        public bool IsValidSudoku(char[][] board)
+        {
+            // 记录行
+            bool[,] row = new bool[9,9];
+            // 记录列
+            bool[,] col = new bool[9,9];
+            // 记录九宫格
+            bool[,] box = new bool[9,9];
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (board[i][j] != '.')
+                    {
+                        int num = board[i][j] - '1';
+                        int boxIndex = i / 3 * 3 + j / 3;
+                        if (row[i,num] || col[j,num] || box[boxIndex,num])
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            row[i,num] = true;
+                            col[j,num] = true;
+                            box[boxIndex,num] = true;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
