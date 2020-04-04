bb87160703ea6aaaf508f668ca123a0dbc8e2f65
凌晨 <lingchen0617@sina.com>
2020-04-04 11:07:15 +0800

    全排列II

---
+++ LeetCode/001-050/048旋转图像/Rotate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public partial class Solution
+    {
+        public void Rotate(int[][] matrix)
+        {
+            int n = matrix.GetLength(0);//找出矩阵的宽度
+            for (int i = 0; i < n / 2; i++)//旋转的起点只有一半（另一半的位置在其他选旋转中会被使用到）
+            {
+                for (int j = i; j < n - i - 1; j++)//y轴同理
+                {
+                    int temp = matrix[i][j];//反向覆盖可以减少利用temp的次数
+                    matrix[i][j] = matrix[n - j - 1][i];
+                    matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                    matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                    matrix[j][n - i - 1] = temp;
+                }
+            }
+        }
+    }
+}
